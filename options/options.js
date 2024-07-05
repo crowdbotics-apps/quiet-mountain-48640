84e9f8a592cd7884477984b3fcee32442da4cfc9
Crowdbotics <team@crowdbotics.com>
2024-07-05 01:19:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quiet-mountain-48640",
+  url: "https://quiet-mountain-48640.botics.co",
+  api: "https://quiet-mountain-48640.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
